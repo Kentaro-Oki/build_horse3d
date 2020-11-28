deba8a48ea4366f6f414f914c0beff6631c400f3
Kentaro-Oki <k010r0ki@gmail.com>
2020-11-28 19:27:53 +0900

    modified a little

--- demo.py
+++ demo.py
@@ -7,20 +7,20 @@ physicsClient = p.connect(p.GUI)#or p.DIRECT for non-graphical version
 p.setAdditionalSearchPath(pybullet_data.getDataPath()) #used by loadURDF
 p.setGravity(0,0,-10)
 planeId = p.loadURDF("plane.urdf")
-horseStartPos = [0,0,1]
-horseStartOrientation = p.getQuaternionFromEuler([0, 0 ,0])
-horseId = p.loadURDF("./urdf/horse3d.urdf", horseStartPos, horseStartOrientation)
+horseStartPos = [0,0,0.5]
+horseStartOrientation = p.getQuaternionFromEuler([0, 0, 0])
+horseId = p.loadURDF("./urdf/trident_snake.urdf", horseStartPos, horseStartOrientation)
 
 # enable joint F/T sensor
 num_joints = p.getNumJoints(horseId)
 for i in range(num_joints):
     p.enableJointForceTorqueSensor(horseId, i)
 
-# # control value
-# maxForce = 100
-# mode = p.POSITION_CONTROL
-# AMP = 0.25 #rad
-# PERIOD = 0.5 # sec
+# control value
+maxForce = 100
+mode = p.POSITION_CONTROL
+AMP = 1.0 #rad
+PERIOD = 1.0 # sec
 timestep = 0
 
 def get_joint_data_array(body_id, joint_id):
@@ -36,13 +36,13 @@ def get_robot_data_array(body_id):
     return robot_data
 
 while True:
-    # target_pos = AMP*np.sin(2*np.pi*1*timestep/(240.*PERIOD)+np.array([0,np.pi/3,2*np.pi/3]))
-    # p.setJointMotorControl2(bodyIndex=boxId, jointIndex=0, targetPosition=target_pos[0], controlMode=mode, force=maxForce)
-    # p.setJointMotorControl2(bodyIndex=boxId, jointIndex=1, targetPosition=target_pos[1], controlMode=mode, force=maxForce)
-    # p.setJointMotorControl2(bodyIndex=boxId, jointIndex=2, targetPosition=target_pos[2], controlMode=mode, force=maxForce)
+    # target_pos = AMP*np.sin(2*np.pi*1*timestep/(240.*PERIOD)+np.array([0,np.pi/3,2*np.pi/3,3*np.pi/3,4*np.pi/3,5*np.pi/3]))
+    # target_vel = AMP*np.cos(2*np.pi*1*timestep/(240.*PERIOD)+np.array([0,np.pi/3,2*np.pi/3,3*np.pi/3,4*np.pi/3,5*np.pi/3]))
+    # target_vel = 0.1
+    # p.setJointMotorControl2(bodyIndex=horseId, jointIndex=0, targetPosition=target_pos[0], targetVelocity=target_vel[0], controlMode=mode, force=maxForce)
     p.stepSimulation()
     horsePos, horseOrn = p.getBasePositionAndOrientation(horseId)
-    print('state:', get_robot_data_array(horseId).shape)
+    # print('state:', get_robot_data_array(horseId).shape)
     time.sleep(1/240.)
     timestep += 1
 
